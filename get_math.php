8fd1e6758b1d8f3ca4aac97af3757d3444fd669f
Jakub Vojacek <jakub@jacon.cz>
2017-03-20 13:45:34 +0100

    fixes

--- get_math.php
+++ get_math.php
@@ -1,10 +1,28 @@
 <?php
 
 
+define("DB_HOST",'localhost');
+define("DB_USERNAME",'maths.cz');
+define("DB_PSW",'Kar4P4JvdwqeeYEC');
+define("DB_NAME", 'maths.cz');
+define("domain", '.maths.cz');
+
+class DATABASE
+{
+    public static function getDb()
+    {
+        $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PSW);
+        $db = mysqli_select_db($connection, DB_NAME);
+        mysqli_query($connection, "SET NAMES UTF8");
+        return $connection;
+    }
+}
 
 $s=$_SERVER["SERVER_NAME"];
 
 
+
+      $db=mysql_select_db("maths.cz",$connect) or die('Nepodaøilo se pøipojit k databázi');
 
       mysqli_query(DATABASE::getDb(), "SET NAMES UTF8");
 
