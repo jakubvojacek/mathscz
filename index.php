15bb35985d99570c6f4ae26f6ca8ffff0ecd9aa4
Jakub Vojacek <jakub@jacon.cz>
2017-03-21 19:25:24 +0100

    fixed style

--- index.php
+++ index.php
@@ -160,10 +160,9 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v2.8";
+  js.src = "//connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v2.8&appId=135212003208377";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-
 
  
 <div id="container">                     
